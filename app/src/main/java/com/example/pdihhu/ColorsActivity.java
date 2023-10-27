43b5132e32628fd1d1f86a93fa27042abaedf186
EdwinR <edwinricardocatalan01@gmail.com>
2023-10-26 23:34:52 -0600

    Sonido de prueba del botón
    Agregué la funcionalidad para reproducir un sonido al momento de presionar el botón

--- app/src/main/java/com/example/pdihhu/ColorsActivity.java
+++ app/src/main/java/com/example/pdihhu/ColorsActivity.java
@@ -2,10 +2,14 @@ package com.example.pdihhu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class ColorsActivity extends AppCompatActivity {
     String Title = "COLORES | YA NJÄT'I";
+
+    ImageButton imgBtnYellow, imgBtnOrange, imgBtnBlue, imgBtnWhite, imgBtnGray, imgBtnBlack, imgBtnRed, imgBtnGreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +17,10 @@ public class ColorsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_colors);
 
         this.setTitle(Title);
+
+        imgBtnYellow = findViewById(R.id.amarillo);
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.ai_chan);
+
+        imgBtnYellow.setOnClickListener(v -> mp.start());
     }
 }
